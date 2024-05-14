d6545325fbaf6000434d26d8053e007405a13cfe
Luis Martin <luisricardomartin@gmail.com>
2024-05-14 09:00:07 -0400

    eliminar carrito

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
-const path = require('path');
-const fs = require('fs');
+const path = require('node:path');
+const fs = require('node:fs');
 const Producto = require('./classes/Producto');
 const Cliente = require('./classes/Cliente');
 const Empleado = require('./classes/Empleado');
@@ -84,17 +84,17 @@ app.post('/buscar-producto', (req, res) => {
         lineas.shift(); // elimina la primera línea (cabecera)
         let foundFlag = false
 
-        lineas.forEach(linea=>{
-            let [id, nombre, categoria, precio] = linea.split(",").map(item=> item.trim())
-            if (id === req.body.id){
+        lineas.forEach(linea => {
+            let [id, nombre, categoria, precio] = linea.split(",").map(item => item.trim())
+            if (id === req.body.id) {
                 let producto = new Producto(id, nombre, categoria, precio)
                 console.log(producto)   //Imprime el producto solicitado por consola
-                                        //(solo para pruebas)
+                //(solo para pruebas)
                 foundFlag = true
                 return res.status(200).send('Producto encontrado') //prueba de estado de respuesta
             }
         })
-        if(!foundFlag){
+        if (!foundFlag) {
             console.log('Producto no encontrado');
             return res.status(404).send('Producto no encontrado') //prueba de estado de respuesta
         }
@@ -111,7 +111,7 @@ app.get('/productos', (req, res) => {
         lineas.shift(); // elimina la primera línea (cabecera)
 
         let productos = lineas.map(linea => {
-            let [id, nombre, categoria, precio] = linea.split(",").map(item=> item.trim())
+            let [id, nombre, categoria, precio] = linea.split(",").map(item => item.trim())
             return new Producto(id, nombre, categoria, precio)
         })
 
@@ -128,23 +128,7 @@ app.get('/catalogo', (req, res) => {
         lineas.shift(); // elimina la primera línea (cabecera)
 
         let productos = lineas.map(linea => {
-            let [id, nombre, categoria, precio] = linea.split(",").map(item=> item.trim())
-            return new Producto(id, nombre, categoria, precio)
-        })
-
-        res.json(productos);
-    });
-});
-
-app.get('/carrito', (req, res) => {
-    fs.readFile(path.join(__dirname, 'files', 'carrito.txt'), 'utf8', (err, data) => {
-        if (err) throw err
-
-        let lineas = data.split('\n'); // divide el contenido por líneas
-        lineas.shift(); // elimina la primera línea (cabecera)
-
-        let productos = lineas.map(linea => {
-            let [id, nombre, categoria, precio] = linea.split(",").map(item=> item.trim())
+            let [id, nombre, categoria, precio] = linea.split(",").map(item => item.trim())
             return new Producto(id, nombre, categoria, precio)
         })
 
@@ -153,14 +137,31 @@ app.get('/carrito', (req, res) => {
 });
 
 //para registrar usuarios en la pagina registrar usuario (entregable usuario)
-app.post('/registrar-usuario', (req, res) => {    
-    if(!req.body.usuario || !req.body.contrasenia || !req.body.cedula || !req.body.telefono || !req.body.direccion) {        
+app.post('/registrar-usuario', (req, res) => {
+    if (!req.body.usuario || !req.body.contrasenia || !req.body.cedula || !req.body.telefono || !req.body.direccion) {
         return res.status(400).send('Todos los campos son requeridos, intente de nuevo');
     }
 
-    const usuario = new Cliente(req.body.nombre,req.body.apellido,req.body.usuario, req.body.contrasenia, req.body.cedula, req.body.direccion, req.body.telefono, req.body.email);
+    const usuario = new Cliente(req.body.nombre, req.body.apellido, req.body.usuario, req.body.contrasenia, req.body.cedula, req.body.direccion, req.body.telefono, req.body.email);
 
     fs.appendFile(path.join(__dirname, 'files', 'usuarios.txt'), usuario.toString(), (err) => {
+        if (err) throw err;
+    });
+
+    const carrito = new carrito(usuario);
+
+    fs.readFile(path.join(__dirname, 'files', 'carrito.txt'), 'utf8', (err, data) => {
+        if (err) throw err;
+
+        const carritos = JSON.parse(data);
+        carritos.push(carrito);
+        const carritosString = JSON.stringify(carritos);
+        fs.writeFile(path.join(__dirname, 'files', 'carrito.txt'), carritosString, (err) => {
+            if (err) throw err;
+        });
+    })
+
+    fs.appendFile(path.join(__dirname, 'files', 'carrito.txt'), usuario.toString(), (err) => {
         if (err) throw err;
     });
 
@@ -174,17 +175,17 @@ app.post('/buscar-usuario', (req, res) => {
     if (!req.body.cedula) {
         return res.status(400).send('El campo cedula es requerido');
     }
-    
+
     fs.readFile(path.join(__dirname, 'files', 'usuarios.txt'), 'utf8', (err, data) => {
         if (err) throw err;
-        
+
         let lineas = data.split('\n'); // divide el contenido por líneas
         lineas.shift(); // elimina la primera línea (cabecera)
         let foundFlag = false
-        
-        lineas.forEach(linea=>{            
-            let [usuario, contrasenia, cedula, telefono, direccion] = linea.split(",").map(item=> item.trim())            
-            if (cedula === req.body.cedula){
+
+        lineas.forEach(linea => {
+            let [usuario, contrasenia, cedula, telefono, direccion] = linea.split(",").map(item => item.trim())
+            if (cedula === req.body.cedula) {
                 let user = new Producto(usuario, contrasenia, cedula, telefono, direccion)
                 console.log(user)   //Imprime el usuario solicitado por consola
                 //(solo para pruebas, no debe imprimir la contraseña)
@@ -192,12 +193,12 @@ app.post('/buscar-usuario', (req, res) => {
                 return res.status(200).send('Usuario encontrado') //prueba de estado de respuesta
             }
         })
-        if(!foundFlag){
+        if (!foundFlag) {
             console.log('Usuario no encontrado');
             return res.status(404).send('Usuario no encontrado') //prueba de estado de respuesta
         }
     })
-    
+
 })
 
 //para buscar productos y registrarlos en el catalogo si existen (entregable catalogo, se buscan por su id)
@@ -213,9 +214,9 @@ app.post('/registrar-producto-catalogo', (req, res) => {
         lineas.shift(); // elimina la primera línea (cabecera)
         let foundFlag = false
 
-        lineas.forEach(linea=>{
-            let [id, nombre, categoria, precio] = linea.split(",").map(item=> item.trim())
-            if (id === req.body.id){
+        lineas.forEach(linea => {
+            let [id, nombre, categoria, precio] = linea.split(",").map(item => item.trim())
+            if (id === req.body.id) {
                 let producto = new Producto(id, nombre, categoria, precio)
                 fs.appendFile(path.join(__dirname, 'files', 'catalogo.txt'), producto.toString(), (err) => {
                     if (err) throw err;
@@ -224,12 +225,102 @@ app.post('/registrar-producto-catalogo', (req, res) => {
                 return res.status(200).send('Producto encontrado') //prueba de estado de respuesta
             }
         })
-        if(!foundFlag){
+        if (!foundFlag) {
             console.log('Producto no encontrado');
             return res.status(404).send('Producto no encontrado') //prueba de estado de respuesta
         }
     })
 
+})
+
+//Ingresar producto al carrito
+app.post('/AddToCart', (req, res) => {
+    const { usuario, idProducto, cantidad } = req.body;
+    fs.readFile(path.join(__dirname, 'files', 'carrito.txt'), (err, data) => {
+        if (err) {
+            console.error(err)
+        }
+        else {
+            const carritos = JSON.parse(data);
+            const carrito = carritos.filter((carrito) => carrito.usuario === usuario)[0];
+            if (carrito) {
+                carrito.productos.push({ idProducto, cantidad });
+            }
+            else {
+                carritos.push({ usuario, productos: [{ idProducto, cantidad }] });
+            }
+            const carritosString = JSON.stringify(carritos);
+            fs.writeFile(path.join(__dirname, 'files', 'carrito.txt'), carritosString, (err) => {
+                if (err) {
+                    console.error(err);
+                }
+            });
+            res.send('Producto agregado al carrito');
+        }
+    })
+})
+
+//Ver carrito
+app.post('/getCart', (req, res) => {
+    const { usuarioSolicitud } = req.body;
+    fs.readFile(path.join(__dirname, 'files', 'carrito.txt'), (err, data) => {
+        if (err) {
+            console.error(err)
+        }
+        else {
+            const carritos = JSON.parse(data);
+            const carrito = carritos.filter((carrito) => carrito.usuario === usuarioSolicitud)[0];
+
+            const carritoString = JSON.stringify(carrito);
+            res.send(carritoString);
+        }
+    })
+})
+
+//Modificar Carrito recibo en el body un usuarioSolicitud
+app.put('/modifyCart', (req, res) => {
+    const { usuarioSolicitud } = req.body;
+    fs.readFile(path.join(__dirname, 'files', 'carrito.txt'), (err, data) => {
+        if (err) {
+            console.error(err)
+        }
+        else {
+            const carritos = JSON.parse(data);
+            const carrito = carritos.filter((carrito) => carrito.usuario === usuarioSolicitud)[0];
+            carrito.productos = carrito.productos.map((producto) => {
+                return { idProducto: producto.idProducto, cantidad: producto.cantidad + 1 };
+            });
+            const carritosString = JSON.stringify(carritos);
+            fs.writeFile(path.join(__dirname, 'files', 'carrito.txt'), carritosString, (err) => {
+                if (err) {
+                    console.error(err);
+                }
+            });
+            res.send('Carrito modificado');
+        }
+    })
+})
+
+//Eliminar producto del carrito
+app.delete('/deleteFromCart', (req, res) => {
+    const { usuarioSolicitud, idProducto } = req.body;
+    fs.readFile(path.join(__dirname, 'files', 'carrito.txt'), (err, data) => {
+        if (err) {
+            console.error(err)
+        }
+        else {
+            const carritos = JSON.parse(data);
+            const carrito = carritos.filter((carrito) => carrito.usuario === usuarioSolicitud)[0];
+            carrito.productos = carrito.productos.filter((producto) => producto.idProducto !== idProducto);
+            const carritosString = JSON.stringify(carritos);
+            fs.writeFile(path.join(__dirname, 'files', 'carrito.txt'), carritosString, (err) => {
+                if (err) {
+                    console.error(err);
+                }
+            });
+            res.send('Producto eliminado del carrito');
+        }
+    })
 })
 
 app.listen(3000, () => {
